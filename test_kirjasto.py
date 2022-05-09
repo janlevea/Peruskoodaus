095cb153ba64f315ad34346c1cacedcc29f221d9
Tuomas Suutari <tuomas@nepnep.net>
2022-05-09 11:05:56 +0300

    Lisää test_kirjasto unittesti

---
+++ test_kirjasto.py
@@ -0,0 +1,62 @@
+import unittest
+
+from kirjasto import Kirja, Luku
+
+
+class TestLaskeSivujenLkm(unittest.TestCase):
+    def test_kaksi_lukua(self):
+        kirja = Kirja("Testikirja", "Kirjoittaja")
+        kirja.lisaa_luku(1, "Eka", sivuja=20)
+        kirja.lisaa_luku(2, "Toka", sivuja=80)
+
+        tulos = kirja.lasku_sivujen_lkm()
+
+        self.assertEqual(tulos, 100)
+
+    def test_ei_yhtaan_lukua(self):
+        kirja = Kirja("Testikirja", "Kirjoittaja")
+
+        tulos = kirja.lasku_sivujen_lkm()
+
+        self.assertEqual(tulos, 0)
+
+    def test_yksi_luku(self):
+        kirja = Kirja("Testikirja", "Kirjoittaja")
+        kirja.lisaa_luku(1, "Luku", sivuja=123)
+
+        tulos = kirja.lasku_sivujen_lkm()
+
+        self.assertEqual(tulos, 123)
+
+
+class TestLisaaLuku(unittest.TestCase):
+    def test_yksi_luku(self):
+        kirja = Kirja("Testikirja", "Kirjoittaja")
+
+        kirja.lisaa_luku(1, "Testiluku", sivuja=5)
+
+        self.assertEqual(len(kirja.luvut), 1)
+        self.assertIsInstance(kirja.luvut[0], Luku)
+        self.assertEqual(kirja.luvut[0].numero, 1)
+        self.assertEqual(kirja.luvut[0].otsikko, "Testiluku")
+        self.assertEqual(kirja.luvut[0].sivuja, 5)
+
+    def test_kaksi_lukua(self):
+        kirja = Kirja("Testikirja", "Kirjoittaja")
+
+        kirja.lisaa_luku(1, "Testiluku", sivuja=5)
+        kirja.lisaa_luku(2, "Toinen luku", sivuja=10)
+
+        self.assertEqual(len(kirja.luvut), 2)
+        self.assertIsInstance(kirja.luvut[0], Luku)
+        self.assertIsInstance(kirja.luvut[1], Luku)
+        self.assertEqual(kirja.luvut[0].numero, 1)
+        self.assertEqual(kirja.luvut[0].otsikko, "Testiluku")
+        self.assertEqual(kirja.luvut[0].sivuja, 5)
+        self.assertEqual(kirja.luvut[1].numero, 2)
+        self.assertEqual(kirja.luvut[1].otsikko, "Toinen luku")
+        self.assertEqual(kirja.luvut[1].sivuja, 10)
+
+
+if __name__ == '__main__':
+    unittest.main()
